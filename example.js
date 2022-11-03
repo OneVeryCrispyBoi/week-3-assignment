f0aff4a9eab0233d6f2acb8bb6fcc4de93135f07
Alexis Lara <alexislaracamacho@my.whccd.edu>
2022-11-02 21:18:33 -0700

    added files

---
+++ example.js
@@ -0,0 +1,8 @@
+//nested if statement
+if (inputNumber % 3 == 0) {
+    console.log("The input number is divisible by 3!");
+    if(inputNumber % 5 == 0) {
+        console.log("The input number is also divisible by 5!");
+        
+    }
+}
---
+++ switch-statements.js
@@ -0,0 +1,14 @@
+let myRating = 2; // 1 - highest, 3 - lowest
+
+// switch statement that checks for rating
+switch (myRating) {
+    case 1 :
+        console.log("You get the highest rating");
+        break;
+    case 2 :
+        console.log("You get an average rating");
+        break;
+    case 3 :
+        console.log("You get the lowest rating");
+        break;
+}
