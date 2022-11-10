f2887d0d81ec72a2812f2905a8766e501b5d501a
Alexis Lara <alexislaracamacho@my.whccd.edu>
2022-11-09 19:47:57 -0800

    stuff

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ for (let index = 2; index <= 100; index + 2) {
 */
 
 // while statement
-
 let areDishesWashed = false;
+let numberOfReminders = 0
 
-while (areDishesWashed) {
+/*
+while (!areDishesWashed) {
     if (!areDishesWashed) {
         console.log("Wash the Dishes!")
+        numberOfReminders++;
+    }
+
+    if (numberOfReminders == 5) {
+        console.log("Nevermind, I'll wash them!");
+        areDishesWashed = true;
     }
 }
+*/
+
+let areDishesWashed2 = true;
+do {
+    console.log("Thanks for washing the dishes!");
+} while (!areDishesWashed2);
