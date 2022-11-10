69c19c7d8461ac32434f3b5ea35950ae68c851f0
Alexis Lara <alexislaracamacho@my.whccd.edu>
2022-11-09 18:56:45 -0800

    stuff

--- script2.js
+++ script2.js
@@ -10,6 +10,20 @@
 // FizzBuzz
 // ...
 
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
+for (let index = 1; i <= 100; index++) {
+    console.log(index);
+
+    if (index % 3 == 0 && index % 5 ==0) {
+        console.log("FizzBuzz");
+    }
+
+    else if (index % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (index % 5 == 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(index);
+    }
 }
