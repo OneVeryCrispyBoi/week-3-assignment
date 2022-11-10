e2968a3b53f3d5049da4ae5f3265eb65da0c6e6e
Alexis Lara <alexislaracamacho@my.whccd.edu>
2022-11-09 19:18:41 -0800

    stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+//1-100
+/*
+for (let index = 2; index <= 100; index + 2) {
+    console.log(index);
+    
+}
+*/
+
+// while statement
+
+let areDishesWashed = false;
+
+while (areDishesWashed) {
+    if (!areDishesWashed) {
+        console.log("Wash the Dishes!")
+    }
+}
